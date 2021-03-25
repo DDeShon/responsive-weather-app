17b608bfdfb8880920d6c71f53b02d12d2e1cdec
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-03-25 04:41:06 -0500

    fixed bug preventing units from showing

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -158,7 +158,8 @@ const createCurrentConditionsDivs = (weatherObj, unit) => {
   const temp = createElem(
     "div",
     "temp",
-    `${Math.round(Number(weatherObj.current.temp))}°`
+    `${Math.round(Number(weatherObj.current.temp))}°`,
+    tempUnit
   );
   const properDesc = toProperCase(weatherObj.current.weather[0].description);
   const desc = createElem("div", "desc", properDesc);
